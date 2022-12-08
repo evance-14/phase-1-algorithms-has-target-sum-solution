342e97a9652ad0ca66f27d23a1acd9ebbb809e9f
evance-14 <kiropevans097@gmail.com>
2022-12-08 15:43:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // create an object to keep track of numbers we have already seen.
+  const seenNumbers = {};
+  // iterate through each number in the array
+  for(let i = 0; i < array.length; i++) {
+    //for the current num, identify a complement that adds to the target (comp = target - num)
+    const complement = target - array[i];
+    // check if any key on our object is the complement
+    // if so return true
+    if (seenNumbers[complement]) return true;
+    // otherwise add that number to the object
+    seenNumbers[array[i]] = true;
+  }
+  // if i reach the end of the array, return false.
+  return false;
 }
 
 /* 
@@ -29,6 +43,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([ 4, 11, 7], 18));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([2, 4, 11, 7], 19));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
